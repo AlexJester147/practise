17aee88441706c03102026f66f19e07a59d0a751
AlexJester147 <vardy2016@lenta.ru>
2021-05-18 14:27:15 +0300

    feat : delete text change

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ console.log(text);
 
 Rect.addEventListener('click', () => {
   navigator.vibrate([100, 100, 200, 200, 300, 300, 400, 400, 500, 500, 600, 600, 700 ,700, 800, 800, 900, 900, 1000, 1000]);
-text.style.display ='none';
+
 });
