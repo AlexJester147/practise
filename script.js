6c4f8ed1bd2881b4b4c7a48c0593c8a6ebdaad6a
AlexJester147 <vardy2016@lenta.ru>
2021-05-18 14:41:18 +0300

    feat : vibrate

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ console.log(text);
 
 
 Rect.addEventListener('click', () => {
-  do {navigator.vibrate([i*100, i*100]);} while (i<10)
+  navigator.vibrate([100, 100, 200, 200, 300, 300, 400, 400, 500, 500, 600, 600, 700 ,700, 800, 800, 900, 900, 1000, 1000]);
 });
