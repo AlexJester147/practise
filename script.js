18fd81292756d90d03da80211204ad2df5132d59
AlexJester147 <vardy2016@lenta.ru>
2021-05-18 14:35:25 +0300

    feat : rewrite vibrate function

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ console.log(text);
 
 Rect.addEventListener('click', () => {
   for (let i=0; i<10; i++){
+    const vibrate = () =>{
   navigator.vibrate([i*100, i*100]);
+  console.log(i);
+};
+vibrate();
 }
 });
