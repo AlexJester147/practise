38052705fe6c63be222fca56f6815cc3ac684614
AlexJester147 <vardy2016@lenta.ru>
2021-05-18 14:25:29 +0300

    feat : add text

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const Rect = document.querySelector('.rect');
-
-console.log(Rect);
+const text = document.querySelector('#text');
+console.log(text);
 
 
 Rect.addEventListener('click', () => {
   navigator.vibrate([100, 100, 200, 200, 300, 300, 400, 400, 500, 500, 600, 600, 700 ,700, 800, 800, 900, 900, 1000, 1000]);
+text.style.display ='none';
 });
